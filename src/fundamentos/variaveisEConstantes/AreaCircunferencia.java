bf6fea699b72b98de7e25246dffa5e917565417c
Lucas Emanuel <lucasemanuelc.rodrigues@gmail.com>
2023-11-15 22:11:52 -0300

    Fundamentos - Variáveis e constantes

--- src/fundamentos/variaveisEConstantes/AreaCircunferencia.java
+++ src/fundamentos/variaveisEConstantes/AreaCircunferencia.java
@@ -1,2 +1,18 @@
-package fundamentos.variaveisEConstantes;public class AreaCircunferencia {
+package fundamentos.variaveisEConstantes;
+
+public class AreaCircunferencia {
+
+    public static void main(String[] args) {
+
+        double raio = 3.4;
+        final double pi = 3.1415;
+
+        double area = pi * raio * raio;
+
+        System.out.println("Area = " + area);
+    }
 }
+/*Anotações:
+- Variável é um espaço na memória alocado para armazenar um determinado tipo.
+- Constante é quando o valor da variável é fixo. Em Java, é definido pela palavra-chave 'final'.
+*/
--- src/fundamentos/variaveisEConstantes/DesafioTemperatura.java
+++ src/fundamentos/variaveisEConstantes/DesafioTemperatura.java
@@ -1,2 +1,18 @@
-package fundamentos.variaveisEConstantes;public class Desafio {
+package fundamentos.variaveisEConstantes;
+
+public class DesafioTemperatura {
+    public static void main(String[] args) {
+        // (F-32)*5/9=C
+
+        final int ajuste = 32;
+        final double fator = 5.0/9;
+        double fahrenheit = 86;
+
+        double calculo1 = fahrenheit - ajuste;
+        double c = calculo1 * fator;
+
+        System.out.println("A conversão = " + c + "c°");
+
+
+    }
 }

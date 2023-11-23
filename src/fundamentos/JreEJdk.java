6b7795e710c09d12c71af30c2579920de6681d51
Lucas Emanuel <lucasemanuelc.rodrigues@gmail.com>
2023-11-22 21:37:41 -0300

    Acrescentei em meus conhecimentos os conceitos sobre o ambiente Java.

---
+++ src/fundamentos/JreEJdk.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class JreEJdk {
+
+/*
+JRE e JDK:
+Nova classe -> Arquivo.java -> compilação -> Arquivo.class
+
+JRE -> Java Runtime Env. -> Usado pelo usuário
+JDK -> Java Development Kit -> Usado pelo desenvolvedor
+
+JVM -> Traduz o código ByteCode <- Arquivo.class
+Java e Javac:
+Javac -> compila .java para .class
+java -> Chama o arquivo .class
+ */
+}

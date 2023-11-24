2682c3990ca2c551594ea55f28e65ce06717fece
Lucas Emanuel <lucasemanuelc.rodrigues@gmail.com>
2023-11-23 21:47:17 -0300

    Aprimorei meus conhecimentos enfrentando um desafio

---
+++ src/fundamentos/exerciciosFundamentos/ConversorFahrenheit.java
@@ -0,0 +1,23 @@
+package fundamentos.exerciciosFundamentos;
+
+import java.util.Scanner;
+
+public class ConversorFahrenheit {
+
+    public static void main(String[] args) {
+        System.out.println("|----------------------------------------|");
+        System.out.println("|  Conversor de Fahrenheit para Celsius  |");
+        System.out.println("|----------------------------------------|");
+
+        Scanner digite = new Scanner(System.in);
+        System.out.print  ("|      Digite o Grau em Fehrenheit:");
+        double grausFah = digite.nextDouble();
+
+        double num1 = grausFah - 32;
+        double resultado = num1 * (9/5);
+
+        System.out.printf("|       %.2f°F equivalem a %.2f°C       \n", grausFah, resultado);
+        System.out.println("|----------------------------------------|");
+    }
+
+}

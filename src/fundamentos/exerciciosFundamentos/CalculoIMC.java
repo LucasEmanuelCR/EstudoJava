15f82a4b6f6f9a4bcd14bbc0a87c748deebf4310
Lucas Emanuel <lucasemanuelc.rodrigues@gmail.com>
2023-11-23 22:32:02 -0300

    Aprimorei meus conhecimentos enfrentando um desafio

--- src/fundamentos/exerciciosFundamentos/CalculoIMC.java
+++ src/fundamentos/exerciciosFundamentos/CalculoIMC.java
@@ -17,7 +17,14 @@ public class CalculoIMC {
         double calculo = Math.pow(altura, 2);
         double calculoImc = peso / calculo;
 
-        System.out.printf("Seu IMC é %.2f", calculoImc);
+        String situcao = calculoImc < 18.5 ? "Baixo Peso" : "Obesidade Móbida";
+        situcao = calculoImc >= 18.5 || calculoImc < 24.9 ? "Peso Normal" : situcao;
+        situcao = calculoImc >= 24.9 || calculoImc < 29.9 ? "Excesso de Peso" : situcao;
+        situcao = calculoImc >= 29.9 || calculoImc < 34.9 ? "Obesidade Classe I" : situcao;
+        situcao = calculoImc >= 34.9 || calculoImc < 39.9 ? "Obesidade Classe II" : situcao;
+
+        System.out.printf("Seu IMC é %.2f\n", calculoImc);
+        System.out.printf("Você está com %s\n", situcao);
 
         informe.close();
     }

fe7cc5ff012b45df139bd8556debdc083fa5a415
Lucas Emanuel <lucasemanuelc.rodrigues@gmail.com>
2023-11-26 12:15:19 -0300

    Ampliei minha proficiência ao incorporar o uso da estrutura 'while' em meus conhecimentos.

---
+++ src/estruturaDeControle/WhileDeterminado.java
@@ -0,0 +1,16 @@
+package estruturaDeControle;
+
+public class WhileDeterminado {
+    public static void main(String[] args) {
+        byte contador = 0;
+        while (contador <= 10){
+            contador++;
+            System.out.println(contador);
+        }
+    }
+}
+/*Anotações:
+    -> 'while' é uma estrutura de repetição.
+    -> A estrutura 'while' é execultada enquando o valor da expressão informada seja true
+    -> 'while' é um estrutura que possui a quantidade de repetições pré-determinada
+ */

875ed0daaa9d737ff5ecb700407989e75bf7dc8c
Lucas Emanuel <lucasemanuelc.rodrigues@gmail.com>
2023-12-05 18:53:01 -0300

    Implantei um exemplo pratico onde um usuário pode criar uma lista de dois produtos que foram comprados, com seu nome valor e desconto.  Esse commit representa minha compreensão sobre classes em java. Na classe Produto estão algumas das minhas anotações referente ao assunto.   Agradeço ao professor Leonardo Moura Leitao, professor do curso "Java 2022 COMPLETO: Do Zero ao Profissional + Projetos!", onde o mesmo deu um ecenlente explicação sobre o assunto

---
+++ src/classe/produto/Produto.java
@@ -0,0 +1,21 @@
+package classe.produto;
+
+// Crio uma classe chamada Produto, com os atributos nome, valor e desconto.
+public class Produto {
+    String nome;
+    double valor;
+    double desconto;
+}
+
+/*Anotações das aulas teóricas 88 até a aula 93:
+    -> Uma classe em Java é declarada com o mesmo nome do arquivo onde ela está contida.
+    -> A anatomia de uma classe é definida da seguinte forma:
+    class NomeDaClasse { Corpo da classe }
+    -> No corpo de uma classe estão seus membros, que consistem em atributos/dados e comportamentos/métodos.
+    -> Uma classe em Java é um tipo personalizado de dados que representa uma abstração da vida real.
+    -> Além disso, uma classe pode ser vista como uma estrutura de dados que define o comportamento e os atributos de um objeto.
+    -> Um objeto, ou instância, é um dado criado por uma classe.
+    -> Membros de uma classe incluem: variáveis, constantes, construtores, métodos e classes internas.
+    -> Existem variáveis, constantes, classes e objetos que pertencem à classe, assim como existem aqueles que pertencem a uma instância específica da classe (ou seja, a um objeto).
+    -> Para acessar um membro da classe ou um membro do objeto, utiliza-se o operador ponto (.), também conhecido como notação ponto.
+*/
---
+++ src/classe/produto/ProdutoTeste.java
@@ -0,0 +1,41 @@
+package classe.produto;
+
+import java.util.Scanner;
+
+public class ProdutoTeste {
+    public static void main(String[] args) {
+        // Utilizo a classe Scanner para receber os dados do usuário
+        Scanner digite = new Scanner(System.in);
+
+        // Instâncio um objeto do tipo Produto e solicito que o usuário realize o cadastro das informações
+        Produto produto1 = new Produto();
+        System.out.println("Informe sua lista de compra");
+        System.out.println("Informações do primeiro produto comprado:  ");
+        System.out.print("Nome: ");
+        produto1.nome = digite.nextLine();
+        System.out.print("Valor: ");
+        produto1.valor = digite.nextDouble();
+        System.out.print("Desconto: ");
+        produto1.desconto = digite.nextDouble();
+        digite.nextLine();
+
+        // Instâncio um segundo objeto do tipo Produto e solicito que o usuário realize o cadastro das informações
+        var produto2 = new Produto();
+        System.out.println("Informações do segundo produto comprado:  ");
+        System.out.print("Nome: ");
+        produto2.nome = digite.nextLine();
+        System.out.print("Valor: ");
+        produto2.valor = digite.nextDouble();
+        System.out.print("Desconto: ");
+        produto2.desconto = digite.nextDouble();
+
+        // Apresento o valor dos produtos listados, a soma do valor bruto e o total após os descontos em cada produto
+        System.out.println("Os prtoodus comprados");
+        System.out.printf("Produtos na lista: %s e %s. \n", produto1.nome, produto2.nome);
+        System.out.printf("Valor bruto da compra: R$%.2f.\n", produto1.valor + produto2.valor);
+        System.out.printf("Valor com os desconto de cada produto: R$%.2f.\n", (produto1.valor * produto1.desconto) + (produto2.valor * produto2.desconto));
+
+        // Fechar o Scanner após o uso para evitar vazamento de recursos
+        digite.close();
+    }
+}

7b9391e0313a9f407d3bc4660f5ddee6224d4c0c
Lucas Emanuel <lucasemanuelc.rodrigues@gmail.com>
2023-12-10 17:48:31 -0300

    Implantei um desafio proposto pelo professor Leonardo Moura Leitao

---
+++ src/classe/PrimeiroTrauma.java
@@ -0,0 +1,14 @@
+package classe;
+
+public class PrimeiroTrauma {
+    // Desfio: Imprimir na tela o valor da variável 'a'.
+
+    int a = 3; // Não pode alterar essa linha
+
+    public static void main(String[] args) {
+
+        PrimeiroTrauma a = new PrimeiroTrauma();
+
+        System.out.println(a.a);
+    }
+}

0691f06b807ad05908ebfc2c634218b15e760ef9
Lucas Emanuel <lucasemanuelc.rodrigues@gmail.com>
2024-01-11 21:19:01 -0300

    Implantei o método de composição de 1 para 1

---
+++ src/oo/composicao/Motor.java
@@ -0,0 +1,15 @@
+package oo.composicao;
+
+public class Motor {
+
+    double fatorInjecao = 1;
+    boolean ligado = false;
+
+    int giros(){
+        if(ligado) {
+            return (int) Math.round(fatorInjecao * 3000);
+        }else{
+            return 0;
+        }
+    }
+}
